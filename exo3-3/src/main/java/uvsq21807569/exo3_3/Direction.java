f1d22cf5cf3a738eb84521854dee98e55d1d3a95
naitalamara <ouali.naitalamara@gmail.com>
2020-03-03 13:57:00 +0100

    ajout classe position et direction

---
+++ exo3-3/src/main/java/uvsq21807569/exo3_3/Direction.java
@@ -0,0 +1,17 @@
+package uvsq21807569.exo3_3;
+
+public class Direction {
+	private float ongle ;
+
+	public Direction(float ongle) {
+		super();
+		this.ongle = ongle;
+	}
+
+	@Override
+	public String toString() {
+		return "Direction [ongle=" + ongle + "]";
+	}
+	
+
+}
---
+++ exo3-3/src/main/java/uvsq21807569/exo3_3/Position.java
@@ -0,0 +1,19 @@
+package uvsq21807569.exo3_3;
+
+public class Position {
+	private int x,y;
+
+	public Position(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Position [x=" + x + ", y=" + y + "]";
+	}
+	
+	
+
+}
